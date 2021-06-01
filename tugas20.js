17c24669e5bea42b723b83b469febe3896b6c8b8
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-06-01 20:49:02 +0700

    Initial commit

---
+++ tugas20.js
@@ -0,0 +1,6 @@
+const searchword = function () {
+    var kalimat = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable."
+    console.log(kalimat.match("are"))
+    console.log(kalimat.match("words"))
+}
+searchword()
